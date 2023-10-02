5b2d5204ee64daf6b18fc2f42b457d9eddf5b152
Cayman <caymannava@gmail.com>
2023-10-02 14:01:07 -0400

    feat: use kubo config file for configuration (#6)
    * feat: use kubo config file for configuration
    
    * chore: update package-lock

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,17 @@
 #! /usr/bin/env node --trace-warnings
 /* eslint-disable no-console */
 
+import { readFile } from 'node:fs/promises'
 import { createServer } from 'node:http'
+import { isAbsolute, join } from 'node:path'
 import { parseArgs } from 'node:util'
 import { noise } from '@chainsafe/libp2p-noise'
 import { yamux } from '@chainsafe/libp2p-yamux'
+import { bootstrap } from '@libp2p/bootstrap'
+import { unmarshalPrivateKey } from '@libp2p/crypto/keys'
 import { kadDHT } from '@libp2p/kad-dht'
 import { mplex } from '@libp2p/mplex'
+import { peerIdFromKeys, peerIdFromString } from '@libp2p/peer-id'
 import { prometheusMetrics } from '@libp2p/prometheus-metrics'
 import { tcp } from '@libp2p/tcp'
 import { webSockets } from '@libp2p/websockets'
@@ -14,6 +19,8 @@ import { createLibp2p } from 'libp2p'
 import { autoNATService } from 'libp2p/autonat'
 import { circuitRelayServer } from 'libp2p/circuit-relay'
 import { register } from 'prom-client'
+import { fromString as uint8ArrayFromString } from 'uint8arrays/from-string'
+import type { PeerId } from '@libp2p/interface/peer-id'
 
 async function main (): Promise<void> {
   const options = {
@@ -56,9 +63,24 @@ async function main (): Promise<void> {
     return
   }
 
+  const configFilename = args.values.config
+  if (configFilename == null) {
+    fatal('--config must be provided')
+  }
+  const configFilepath = isAbsolute(configFilename) ? configFilename : join(process.cwd(), configFilename)
+  const config = await readConfig(configFilepath)
+
+  const peerId = await decodePeerId(config.Identity.PrivKey)
+  if (!peerIdFromString(config.Identity.PeerId).equals(peerId)) {
+    fatal('Config Identity.PeerId doesn\'t match Identity.PrivKey')
+  }
+
   const services: Record<string, any> = {
     relay: circuitRelayServer({
       advertise: true
+    }),
+    bootstrap: bootstrap({
+      list: config.Bootstrap
     })
   }
 
@@ -71,6 +93,12 @@ async function main (): Promise<void> {
   }
 
   const node = await createLibp2p({
+    peerId,
+    addresses: {
+      listen: config.Addresses.Swarm,
+      announce: config.Addresses.Announce,
+      noAnnounce: config.Addresses.NoAnnounce
+    },
     transports: [
       webSockets(),
       tcp()
@@ -105,6 +133,59 @@ async function main (): Promise<void> {
 }
 
 main().catch(err => {
-  console.error(err) // eslint-disable-line no-console
-  process.exit(1)
+  fatal(err)
 })
+
+function fatal (msg?: any): never {
+  console.error(msg)
+  process.exit(1)
+}
+
+/** Subset of options that we care about from an kubo config */
+interface KuboConfig {
+  Bootstrap: string[]
+  Addresses: {
+    Swarm: string[]
+    Announce: string[]
+    NoAnnounce: string[]
+  }
+  Identity: {
+    PeerId: string
+    PrivKey: string
+  }
+}
+
+function validateKey (config: any, key: string, path: string): void {
+  if (config[key] == null) {
+    fatal(`Config key missing: ${path}`)
+  }
+}
+
+function validateConfig (config: any): config is KuboConfig {
+  validateKey(config, 'Bootstrap', 'Bootstrap')
+  validateKey(config, 'Addresses', 'Addresses')
+  validateKey(config.Addresses, 'Swarm', 'Addresses.Swarm')
+  validateKey(config.Addresses, 'Announce', 'Addresses.Announce')
+  validateKey(config.Addresses, 'NoAnnounce', 'Addresses.NoAnnounce')
+  validateKey(config, 'Identity', 'Identity')
+  validateKey(config.Identity, 'PeerId', 'Identity.PeerId')
+  validateKey(config.Identity, 'PrivKey', 'Identity.PrivKey')
+  return true
+}
+
+async function readConfig (filepath: string): Promise<KuboConfig> {
+  const configString = await readFile(filepath, 'utf8')
+  const config = JSON.parse(configString)
+  validateConfig(config)
+  return config
+}
+
+async function decodePeerId (privkeyStr: string): Promise<PeerId> {
+  try {
+    const privkeyBytes = uint8ArrayFromString(privkeyStr, 'base64pad')
+    const privkey = await unmarshalPrivateKey(privkeyBytes)
+    return await peerIdFromKeys(privkey.public.bytes, privkey.bytes)
+  } catch (e) {
+    fatal('Invalid peer-id private key')
+  }
+}
