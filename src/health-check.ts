dfed9dccd5f9398a21d95dffeb21f22bfc91f123
achingbrain <alex@achingbrain.net>
2024-09-23 18:22:56 +0100

    chore: remove unused code

--- src/health-check.ts
+++ src/health-check.ts
@@ -13,49 +13,18 @@
  * as used by https://github.com/libp2p/rust-libp2p/tree/master/misc/server
  */
 import { readFileSync } from 'node:fs'
-import { parseArgs } from 'node:util'
 import { noise } from '@chainsafe/libp2p-noise'
 import { yamux } from '@chainsafe/libp2p-yamux'
-import { peerIdFromString } from '@libp2p/peer-id'
 import { tcp } from '@libp2p/tcp'
 import { webSockets } from '@libp2p/websockets'
 import { multiaddr, type Multiaddr } from '@multiformats/multiaddr'
 import { createLibp2p } from 'libp2p'
 import type { PeerId } from '@libp2p/interface'
 
-function readPositional (): PeerId | Multiaddr | undefined {
-  const { positionals } = parseArgs({
-    allowPositionals: true
-  })
-
-  if (positionals.length === 0) {
-    return
-  }
-
-  if (positionals.length > 1) {
-    throw new Error('Multiple peerIds nor multiaddrs are not supported')
-  }
-
-  try {
-    return peerIdFromString(positionals[0])
-  } catch {
-    try {
-      return multiaddr(positionals[0])
-    } catch {}
-  }
-}
-
-function readListeningAddrs (): Multiaddr[] {
-  try {
-    // read from file
-    return readFileSync('listening-addrs.txt', 'utf8')
-      .split('\n')
-      .map(ma => multiaddr(ma))
-  } catch (err) {
-    console.error('Could read listening-addrs.txt', err)
-    process.exit(1)
-  }
-}
+// load the addresses the bootstrapper is listening on
+const target: PeerId | Multiaddr | Multiaddr[] = readFileSync('listening-addrs.txt', 'utf8')
+  .split('\n')
+  .map(ma => multiaddr(ma))
 
 // we will use this node to dial the bootstrapper
 const node = await createLibp2p({
@@ -70,8 +39,6 @@ const node = await createLibp2p({
     noise()
   ]
 })
-
-const target: PeerId | Multiaddr | Multiaddr[] = readPositional() ?? readListeningAddrs()
 
 console.info('Trying to dial', target)
 
