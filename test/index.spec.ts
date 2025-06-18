0faae848cd3567e8b1b2b3354df6ab3aba92367f
achingbrain <alex@achingbrain.net>
2025-06-18 17:29:31 +0300

    chore: fix deps

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,5 +1,3 @@
-import { execa } from 'execa'
-
 describe('amino DHT bootstrapper', () => {
   let controller: AbortController
 
@@ -12,27 +10,6 @@ describe('amino DHT bootstrapper', () => {
   })
 
   it('should start the bootstrapper', async () => {
-    const config = {
-      bootstrap: {
-        list: [
-          "/dns4/am6.bootstrap.libp2p.io/tcp/443/wss/p2p/QmbLHAnMoJPWSCR5Zhtx6BHJX9KiKNN6tpvbUcqanj75Nb",
-          "/dns4/sg1.bootstrap.libp2p.io/p2p/QmcZf59bWwK5XFi76CZX8cbJ4BhTzzA3gU1ZjYZcYW3dwt",
-          "/dns4/sv15.bootstrap.libp2p.io/p2p/QmNnooDu7bfjPFoTZYxMNLWUQJyrVwtbZg5gBMjTezGAJN"
-        ]
-      }
-    }
 
-    const proc = execa('node', ['dist/src/index.js'], {
-      cancelSignal: controller.signal
-    })
-
-    proc.stdout.on('data', buf => {
-      console.info(buf.toString())
-    })
-    proc.stderr.on('data', buf => {
-      console.error(buf.toString())
-    })
-
-    await proc
   })
 })
