6e1d5bea14938d124e2f5a5818112497e9e484f1
achingbrain <alex@achingbrain.net>
2025-07-07 09:12:35 +0200

    fix: record quic v1 connections

--- src/services/connections-by-transport-metrics.ts
+++ src/services/connections-by-transport-metrics.ts
@@ -1,4 +1,4 @@
-import { WebSockets, WebRTC, WebRTCDirect, WebTransport, WebSocketsSecure, TCP, QUIC, Circuit } from '@multiformats/multiaddr-matcher'
+import { WebSockets, WebRTC, WebRTCDirect, WebTransport, WebSocketsSecure, TCP, QUIC, Circuit, QUICV1 } from '@multiformats/multiaddr-matcher'
 import type { Libp2pEvents, Metrics, TypedEventTarget } from '@libp2p/interface'
 
 interface Components {
@@ -34,6 +34,8 @@ export function connectionsByTransportMetrics (): (components: Components) => un
         metrics.increment({ TCP: true })
       } else if (QUIC.matches(conn.remoteAddr)) {
         metrics.increment({ QUIC: true })
+      } else if (QUICV1.matches(conn.remoteAddr)) {
+        metrics.increment({ QUIC_V1: true })
       } else if (Circuit.matches(conn.remoteAddr)) {
         metrics.increment({ CircuitRelay: true })
       }
@@ -56,6 +58,8 @@ export function connectionsByTransportMetrics (): (components: Components) => un
         metrics.decrement({ TCP: true })
       } else if (QUIC.matches(conn.remoteAddr)) {
         metrics.decrement({ QUIC: true })
+      } else if (QUICV1.matches(conn.remoteAddr)) {
+        metrics.decrement({ QUIC_V1: true })
       } else if (Circuit.matches(conn.remoteAddr)) {
         metrics.decrement({ CircuitRelay: true })
       }
